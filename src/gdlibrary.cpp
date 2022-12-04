4f17f596623538bdbfa7c384343f27cfce102799
ghsoares <ghsoares99795@gmail.com>
2022-12-04 18:39:17 -0300

    Fresh start for engine vibration model

--- src/gdlibrary.cpp
+++ src/gdlibrary.cpp
@@ -1,9 +1,3 @@
-#include "engine_config.h"
-#include "engine_audio_generator.h"
-#include "engine_audio_recorder.h"
-#include "procedural_engine_audio.h"
-#include "engine_audio_player.h"
-
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
 	godot::Godot::gdnative_init(o);
 }
@@ -14,12 +8,4 @@ extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_opt
 
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
 	godot::Godot::nativescript_init(handle);
-
-	godot::register_class<godot::EngineConfig>();
-	godot::register_class<godot::EngineCylinderConfig>();
-	godot::register_class<godot::EngineMufflerConfig>();
-	godot::register_class<godot::EngineAudioGenerator>();
-	godot::register_class<godot::EngineAudioRecorder>();
-	godot::register_class<godot::ProceduralEngineAudioGenerator>();
-	godot::register_class<godot::EngineAudioPlayer>();
 }
